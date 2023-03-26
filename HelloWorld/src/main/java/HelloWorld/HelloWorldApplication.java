a3ebe5ffe9b934ffb971d22c43398044b5217f97
late-en <996446269@qq.com>
2023-03-26 15:39:11 +0800

    hello world finished

---
+++ HelloWorld/src/main/java/HelloWorld/HelloWorldApplication.java
@@ -0,0 +1,27 @@
+package HelloWorld;
+
+import cn.hutool.core.util.StrUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 邓家鑫
+ */
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(required = false, name = "who") String who){
+        if (StrUtil.isBlank(who)) {
+            who = "World";
+        }
+        return StrUtil.format("Hello, {}!", who);
+    }
+}
