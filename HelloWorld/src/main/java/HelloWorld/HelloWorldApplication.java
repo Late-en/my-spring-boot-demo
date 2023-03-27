042997699557f7451a550c960dde2a20781535f4
late-en <996446269@qq.com>
2023-03-27 22:52:06 +0800

    hello world 路由配置修改

--- HelloWorld/src/main/java/HelloWorld/HelloWorldApplication.java
+++ HelloWorld/src/main/java/HelloWorld/HelloWorldApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author 邓家鑫
+ * @author Late-en
  */
 @SpringBootApplication
 @RestController
