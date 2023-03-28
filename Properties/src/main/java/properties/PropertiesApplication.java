875aafb509ecee5e7424ef36d8c7c5787a088633
late-en <996446269@qq.com>
2023-03-28 21:57:34 +0800

    properties 添加注释

--- Properties/src/main/java/properties/PropertiesApplication.java
+++ Properties/src/main/java/properties/PropertiesApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author Late-en
+ * spring-boot-configuration-processor的作用就是将自己创建的配置类生成元数据信息
+ * 生成文件位置（target/classes/META-INF/spring-configuration-metadata.json）
+ * 这样就能在你自己的配置文件(main/resources/application.yml)中有了显现提示
  */
 
 @SpringBootApplication
